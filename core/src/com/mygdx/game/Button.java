3c9c86bc2746ca383bbc94b234748da53d49f2dd
Unknown <up201605330@g.uporto.pt>
2018-04-30 21:11:42 +0100

    Added Buttons

---
+++ core/src/com/mygdx/game/Button.java
@@ -0,0 +1,18 @@
+package com.mygdx.game;
+
+/**
+ * A model representing a button.
+ */
+public class Button extends Body{
+
+    /**
+     * Constructor of the Button
+     *
+     * @param x the x Coordinate
+     * @param y the y Coordinate
+     */
+    public Button(float x, float y){
+
+        super(x, y);
+    }
+}
---
+++ core/src/com/mygdx/game/ButtonView.java
@@ -0,0 +1,34 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+/**
+ * A view representing a ball
+ */
+public class ButtonView extends BodyView{
+
+    /**
+     * Constructor of the button view
+     *
+     * @param game the game.
+     */
+    public ButtonView(FireBoyWaterGirl game) {
+
+        super(game);
+    }
+
+    /**
+     * Abstract method creates the sprites for all the buttons.
+     *
+     * @param game the game.
+     *
+     * @return the sprite representing the button.
+     * */
+    @Override
+    public Sprite createSprite(FireBoyWaterGirl game) {
+
+        Texture texture = game.getAssetManager().get("button.png");
+        return new Sprite(texture, texture.getWidth(), texture.getHeight());
+    }
+}
