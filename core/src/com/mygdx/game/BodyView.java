d69aa14d4ba845627ac083975bca2021fec9237d
Unknown <migud.carvalho@gmail.com>
2018-04-30 14:20:58 +0100

    updated views

--- core/src/com/mygdx/game/BodyView.java
+++ core/src/com/mygdx/game/BodyView.java
@@ -1,4 +1,14 @@
 package com.mygdx.game;
 
-public class BodyView {
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public abstract class BodyView {
+
+    Sprite sprite;
+    
+    public BodyView(FireBoyWaterGirl game){
+        sprite = createSprite(game);
+    }
+
+    public abstract Sprite createSprite(FireBoyWaterGirl game);
 }
--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -1,24 +1,70 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-
+/**
+  * Game screen. Draws all the views of all objects of the game.
+  */
 public class GameScreen extends ScreenAdapter{
 
+    /**
+     * The game.
+     */
     FireBoyWaterGirl fbwg;
+
+    /**
+     * The game data.
+     */
     GameModel model;
 
+    /**
+     * Creates the screen.
+     *
+     * @param fbwg The game
+     * @param model The model to be drawn
+     */
     public GameScreen(FireBoyWaterGirl fbwg, GameModel model) {
-
         this.fbwg = fbwg;
         this.model = model;
 
         loadImages();
     }
 
+    /**
+     * loads the images used in this screen
+     */
     public void loadImages(){
 
         this.fbwg.getAssetManager().load("coise.jpg0", Texture.class);
+        this.fbwg.getAssetManager().finishLoading();
+    }
+
+    /**
+     * renders this screen
+     *
+     * @param delta time in seconds since last render
+     */
+    @Override
+    public void render(float delta) {
+        //camera updates se quiseremos usar camara.
+
+        //clear screen
+        Gdx.gl.glClearColor(103 / 255f, 69 / 255f, 117 / 255f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);      // valores certos?
+
+        fbwg.getSpriteBatch().begin();
+        //drawBackground
+        //drawObjects
+        fbwg.getSpriteBatch().end();
+    }
+
+    /**
+     * Draw objects on the screen
+     */
+    private void drawObjects(){
+        
     }
 }
