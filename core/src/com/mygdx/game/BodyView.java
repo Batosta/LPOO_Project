1ab43a9b3734f810d52590b38d9b2a8e79aee2cd
Unknown <migud.carvalho@gmail.com>
2018-04-30 19:52:55 +0100

    bodyview

--- core/src/com/mygdx/game/BodyView.java
+++ core/src/com/mygdx/game/BodyView.java
@@ -5,7 +5,15 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 public abstract class BodyView {
 
     Sprite sprite;
-    
+
+     /**
+      * Abstract method creates the sprites for all the objects.
+      *
+      * @param game the game.
+      *
+      * @return the sprite representing this view.
+      */
+
     public BodyView(FireBoyWaterGirl game){
         sprite = createSprite(game);
     }
