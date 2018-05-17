4dd67ad8dade74f4f61459b14ab7c18694d4d32a
Unknown <up201605330@g.uporto.pt>
2018-05-17 18:38:06 +0100

    Tiled Map Editor changes

---
+++ core/assets/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="10" tileheight="10" tilecount="875" columns="35">
+ <image source="tileset.png" width="350" height="250"/>
+</tileset>
